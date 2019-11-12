1e9e457024f313ec86df5d8b370f5a49546ff74c
diproger <diprogerus@gmail.com>
2019-11-12 18:54:22 +0500

    First commit

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { createMessage } from './messsages'
+import {GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS} from './types';
+
+// GET LEADS ACTIONS
+
+export const getLeads = () => dispatch => {
+     axios.get('/api/leads/')
+        .then(res => {
+            dispatch({
+                type:GET_LEADS,
+                payload:res.data
+            });
+        }).catch(err => console.log(err))
+}
+
+// DELETE LEAD
+
+export const deleteLead = (id) => dispatch => {
+    axios.delete(`/api/leads/${id}/`)
+       .then(res => {
+           dispatch(createMessage({deleteLead:"Lead Deleted"}))
+           dispatch({
+               type:DELETE_LEAD,
+               payload:id
+           });
+       }).catch(err => console.log(err))
+}
+
+// ADD LEAD
+
+export const addLead = (lead) => dispatch => {
+    console.log(lead);
+    
+    axios.post('/api/leads/',lead)
+       .then(res => {
+        dispatch(createMessage({addLead:"Lead added"}))
+           dispatch({
+               type:ADD_LEAD,
+               payload:res.data
+           });
+       }).catch(err => {
+           const errors = {
+               msg: err.response.data,
+               status: err.response.status
+           }
+           dispatch({
+               type: GET_ERRORS,
+               payload:errors
+           })
+       })
+}
---
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -0,0 +1,47 @@
+import React, { Component, Fragment } from 'react'
+import { withAlert } from "react-alert"
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import errors from '../../reducers/errors'
+
+
+export class Alerts extends Component {
+
+    static propTypes = {
+        error: PropTypes.object.isRequired,
+        message: PropTypes.object
+    }
+
+    componentDidUpdate(prevProps){
+        const {error, alert, message} = this.props;
+        if (error !== prevProps.error){
+            if(error.msg.name){
+                alert.show(`Name: ${error.msg.name.join()}`)
+            }
+            if(error.msg.email){
+                alert.show(`Email: ${error.msg.email.join()}`)
+            }
+            if(error.msg.message){
+                alert.show(`Message: ${error.msg.message.join()}`)
+            }
+
+        }
+
+        if(message !== prevProps.message){
+            if(message.deleteLead){ alert.success(message.deleteLead)}
+            if(message.addLead){ alert.success(message.addLead)}
+
+        }
+    }
+
+    render() {
+        return <Fragment />;
+    }
+}
+
+const mapStateToProps = state => ({
+    error: state.errors,
+    message: state.messages
+})
+
+export default connect(mapStateToProps)( withAlert()(Alerts))
